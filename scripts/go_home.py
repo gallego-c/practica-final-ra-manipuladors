6e492a513cd6fae6a934b66d3b94345f379ef301
Stargix <sergi.flores@estudiantat.upc.edu>
2026-06-12 17:14:21 +0200

    Smooth go_home speed and increase travel wait time to 8.0s

--- scripts/go_home.py
+++ scripts/go_home.py
@@ -7,8 +7,8 @@ import math
 HOST = "10.10.73.239"   # IP del controlador del UR
 PORT = 30002            # Secondary client interface
 
-ACC   = 0.8     # aceleracion articular [rad/s^2]
-VEL   = 0.6     # velocidad articular   [rad/s]
+ACC   = 0.3     # aceleracion articular suave [rad/s^2]
+VEL   = 0.3     # velocidad articular suave   [rad/s]
 
 # HOME pose: Primera posicion del path tilt_x_ur3 (en radianes)
 home_config = [0.844914, -0.987856, 0.536689, -1.13289, -1.57184, 0.892212]
@@ -23,12 +23,12 @@ def go_home():
     q_str = "[" + ", ".join(str(x) for x in home_config) + "]"
     program = f"def home():\n  movej({q_str}, a={ACC}, v={VEL})\nend\nhome()\n"
     
-    print("Moviendo a la pose HOME...")
+    print("Moviendo a la pose HOME de forma suave...")
     print(program)
     sock.send(program.encode())
     
-    # Esperar a que termine el movimiento
-    time.sleep(5.0)
+    # Esperar lo suficiente para que el movimiento termine por completo
+    time.sleep(8.0)
     
     sock.close()
     print("Robot en pose HOME y conexion cerrada.")
