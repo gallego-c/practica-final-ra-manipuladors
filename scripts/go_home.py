d18aeba43da9e4cfeb25c6f5eff20f092179ee3e
Stargix <sergi.flores@estudiantat.upc.edu>
2026-06-12 16:36:08 +0200

    Update HOME pose in go_home.py to be the first position of tilt_x_ur3

--- scripts/go_home.py
+++ scripts/go_home.py
@@ -10,8 +10,8 @@ PORT = 30002            # Secondary client interface
 ACC   = 0.8     # aceleracion articular [rad/s^2]
 VEL   = 0.6     # velocidad articular   [rad/s]
 
-# HOME pose: [0, -pi/2, 0, -pi/2, 0, 0]
-home_config = [0.0, -math.pi/2, 0.0, -math.pi/2, 0.0, 0.0]
+# HOME pose: Primera posicion del path tilt_x_ur3 (en radianes)
+home_config = [0.844914, -0.987856, 0.536689, -1.13289, -1.57184, 0.892212]
 
 def go_home():
     print(f"Conectando al robot en {HOST}:{PORT} para ir a HOME...")
